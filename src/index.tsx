e4305b219fd8cd58bb3703157a607d1a48fec259
nikita <freenikita95@gmail.com>
2021-01-14 01:01:45 +0200

    add project

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+import './index.css';
+import { theme } from './theme'
+
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
